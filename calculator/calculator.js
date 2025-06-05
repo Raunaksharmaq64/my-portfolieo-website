9a2e99886437eef2325f69b9decf136aa1a3b0a1
raunaksharmaq64 <raunaksharma88630mt@gmail.com>
2025-06-05 19:41:00 +0530

    adding new project 4 section and attach link of calculator

---
+++ calculator/calculator.js
@@ -0,0 +1,35 @@
+gsap.from('.heading',{
+    y:-1000,
+    delay:1,
+    duration:3,
+    
+});
+gsap.from('#heading',{
+    y:0,
+    delay:3,
+    duration:2,
+    scale:0,
+    
+});
+
+
+const inputBox = document.querySelector(".input-box");
+const buttons = document.querySelectorAll(".buttons");
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const value = button.textContent;
+
+    if (value === "CLEAR") {
+      inputBox.value = "";
+    } else if (value === "=") {
+      try {
+        inputBox.value = eval(inputBox.value) || "";
+      } catch {
+        inputBox.value = "Error";
+      }
+    } else {
+      inputBox.value += value;
+    }
+  });
+});
